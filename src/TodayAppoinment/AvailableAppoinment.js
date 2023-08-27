c278d5b55abfe5770131def1f143048430c6dbf6
munir79 <jakirhossainmunir79@gmail.com>
2023-08-27 15:05:55 +0600

    use modal

--- src/TodayAppoinment/AvailableAppoinment.js
+++ src/TodayAppoinment/AvailableAppoinment.js
@@ -1,12 +1,34 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import AppoinmentOption from './AppoinmentOption';
+import OpenModal from './OpenModal';
 
 const AvailableAppoinment = ({selectedDate}) => {
+    const [appointmentOption,setAppoinmentOption]=useState([]);
+    const [treatment,settreatment]=useState(null);
+    useEffect( ()=>{
+        fetch('Service.json')
+        .then(res=>res.json())
+        .then(data=>setAppoinmentOption(data))
+    },[])
     
     return (
-        <div>
-            <h4 className='text-center text-primary text-bold text-2xl'> You Have Selected Date:{format(selectedDate, 'PP')}  </h4>
-        </div>
+        <section className='mt-16'>
+             <h4 className='text-center text-info text-bold '> You Have Selected Date:{format(selectedDate, 'PP')}  </h4>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3 mt-10'>
+            {
+                appointmentOption.map(data=><AppoinmentOption key={data.id} 
+                    data={data} 
+                    settreatment={settreatment}
+                    ></AppoinmentOption>)
+             }
+            </div>
+         
+         {
+            treatment &&
+            <OpenModal treatment={treatment} ></OpenModal>
+         }
+        </section>
     );
 };
 
